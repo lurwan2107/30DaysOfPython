0cdd156067321f3df67b2058ddd8cbe1ded22217
Abdullahi Lurwanu <csc180680@students.umyu.edu.ng>
2023-02-21 18:12:02 +0100

    update

---
+++ Day_017/Exercise_D17.py
@@ -0,0 +1,8 @@
+# Day 17 UNPACKING
+
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+*nordic_contries, es, ru = names
+
+print(nordic_contries)
+print(es)
+print(ru)
